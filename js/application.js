14d361ee6fe3c3eb7dfdabac2928683b2f864468
ianpetzer <ianpetzer@gmail.com>
2013-05-09 23:03:01 +0200

    Fixed package name

--- js/application.js
+++ js/application.js
@@ -1,25 +1,26 @@
+App.reopen({
 
-Inet.phoneGapApp = {
+  phoneGapApp: {
     // Application Constructor
-    initialize: function() {
-        this.bindEvents();
+    initialize: function () {
+      this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    bindEvents: function () {
+      document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        Inet.phoneGapApp.receivedEvent('deviceready');
+    onDeviceReady: function () {
+      App.phoneGapApp.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
 //            var parentElement = document.getElementById(id);
 //            var listeningElement = parentElement.querySelector('.listening');
 //            var receivedElement = parentElement.querySelector('.received');
@@ -27,6 +28,7 @@ Inet.phoneGapApp = {
 //            listeningElement.setAttribute('style', 'display:none;');
 //            receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+      console.log('Received Event: ' + id);
     }
-};
+  }
+});
